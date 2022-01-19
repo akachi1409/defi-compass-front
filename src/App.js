fee4b5592e5066dec2e37b33e90e7b7877d64fa3
trusteddev1325 <trusteddev1325@gmail.com>
2022-01-19 18:47:45 +0900

    add footer and update json

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import logo from './logo.svg';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-import Author from './components/author/author.js'
+import Author from './components/author/author.js';
+import Footer from './components/basic/Footer/footer.js';
 function App() {
   return (
     <div className="App">
       test
       <Author/>
+      <Footer/>
     </div>
   );
 }
